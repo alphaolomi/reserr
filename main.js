49648a576607abf3e2c3c32ed59675be0d0a5a01
Alpha Olomi <alphaolomi@gmail.com>
2019-12-15 01:40:14 +0300

    feat: lint with prettier

--- main.js
+++ main.js
@@ -1,16 +1,18 @@
-const {app, BrowserWindow} = require('electron')
-const url = require('url')
-const path = require('path')
+const { app, BrowserWindow } = require("electron");
+const url = require("url");
+const path = require("path");
 
-let win
+let win;
 
 function createWindow() {
-   win = new BrowserWindow({width: 800, height: 600})
-   win.loadURL(url.format ({
-      pathname: path.join(__dirname, 'index.html'),
-      protocol: 'file:',
+  win = new BrowserWindow({ width: 800, height: 600 });
+  win.loadURL(
+    url.format({
+      pathname: path.join(__dirname, "index.html"),
+      protocol: "file:",
       slashes: true
-   }))
+    })
+  );
 }
 
-app.on('ready', createWindow)
+app.on("ready", createWindow);
